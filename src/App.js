8b57498bf9af9b0d34b15ee6a0ea69831a3b1ab7
bravelhino <saparov20003@mail.ru>
2021-08-26 22:13:37 +0600

    connect eslint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import styled from "styled-components";
-import ContactListComponent from "./components/ContactListComponent";
-import ConversationComponent from "./components/ConversationComponent";
-import {useEffect, useState} from "react";
-import Login from "./components/Login";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth, getAllUsers, getChats} from "./config/firebase";
-import "./App.css"
-import NewContacts from "./components/NewContacts";
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import ContactListComponent from './components/ContactListComponent';
+import ConversationComponent from './components/ConversationComponent';
+import Login from './components/Login';
+import { auth, getAllUsers, getChats } from './config/firebase';
+import './App.css';
+import NewContacts from './components/NewContacts';
+
 const Container = styled.div`
   display: flex;
   flex-direction: row;
   height: 100vh;
   width: 100%;
   background: #f8f9fb;
-`
+`;
 const Placeholder = styled.div`
   flex: 3;
   display: flex;
@@ -27,53 +28,86 @@ const Placeholder = styled.div`
     font-size: 32px;
     color: #525252;
   }
-`
+`;
 const ChatPlaceholder = styled.img`
   width: 240px;
   height: 240px;
   border-radius: 50%;
   object-fit: contain;
-`
-
+`;
 
 function App() {
-  const [selectedChat, setChat] = useState()
+  const [selectedChat, setChat] = useState();
   const [user, loading] = useAuthState(auth);
   const [newContactsShow, setNewContactsShow] = useState(false);
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   const [chats, setChats] = useState();
   const [update, setUpdate] = useState(false);
   useEffect(() => {
     getAllUsers().then((data) => {
-      setUsers(data)
-    })
+      setUsers(data);
+    });
     getChats(setChats);
-  }, [])
+  }, []);
 
-  if(loading) {
+  if (loading) {
     return (
       <>
         <div className="lds-hourglass">
-          <div style={{color: '#000', position: 'absolute', bottom: -50, left: 1, fontWeight: 'bold', fontSize: 18}}>Loading...</div>
+          <div style={{
+            color: '#000', position: 'absolute', bottom: -50, left: 1, fontWeight: 'bold', fontSize: 18,
+          }}
+          >
+            Loading...
+          </div>
         </div>
       </>
-    )
+    );
   }
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <>
       {
         user ? (
           <Container>
             {
-              newContactsShow ?
-                <NewContacts user={user} chats={chats} users={users} setChat={setChat} setNewContactsShow={setNewContactsShow} selectedChat={selectedChat} setUpdate={setUpdate}/> :
-                <ContactListComponent setUpdate={setUpdate} setNewContactsShow={setNewContactsShow} setChat={setChat} user={user}/>
+              newContactsShow
+                ? (
+                  <NewContacts
+                    user={user}
+                    chats={chats}
+                    users={users}
+                    setChat={setChat}
+                    setNewContactsShow={setNewContactsShow}
+                    selectedChat={selectedChat}
+                    setUpdate={setUpdate}
+                  />
+                )
+                : (
+                  <ContactListComponent
+                    setUpdate={setUpdate}
+                    setNewContactsShow={setNewContactsShow}
+                    setChat={setChat}
+                    user={user}
+                  />
+                )
             }
-            {selectedChat ? <ConversationComponent setChat={setChat} update={update} setUpdate={setUpdate} user={user} selectedChat={selectedChat} /> : <Placeholder>
-              <ChatPlaceholder src={"/intro-connection-hq-light_9466a20e6d2921a21ac7ab82419be157.jpg"}/>
-              <span>Keep your phone connected</span>
-              WhatsApp connects to your phone to sync messages.
-            </Placeholder> }
+            {selectedChat
+              ? (
+                <ConversationComponent
+                  setChat={setChat}
+                  update={update}
+                  setUpdate={setUpdate}
+                  user={user}
+                  selectedChat={selectedChat}
+                />
+              ) : (
+                <Placeholder>
+                  <ChatPlaceholder src="/intro-connection-hq-light_9466a20e6d2921a21ac7ab82419be157.jpg" />
+                  <span>Keep your phone connected</span>
+                  WhatsApp connects to your phone to sync messages.
+                </Placeholder>
+              ) }
           </Container>
         ) : (
           <Login />
